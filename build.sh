3d76502fd991833b7d67317eabdfaee21763ae81
Latte <73871195+Calliope-K@users.noreply.github.com>
2021-01-05 20:32:46 +0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#bin!bash
+
+#cvirontment
+sudo apt-get install flex bison ncurses-dev texinfo gcc gperf patch libtool automake g++ libncurses5-dev gawk subversion expat libexpat1-dev python-all-dev binutils-dev libgcc1:i386 bc libcloog-isl-dev libcap-dev autoconf autoconf-archive libgmp-dev build-essential gcc-multilib g++-multilib pkg-config libmpc-dev libmpfr-dev autopoint gettext liblzma-dev libssl-dev libz-dev
+
+#cloning
+git clone https://github.com/Calliope-K/build-tools-gcc
+cd build-tools-gcc
+
+#start
+./build -a arm64 -s linaro -v 7 -V
