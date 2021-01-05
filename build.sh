55faa2b98acd4932ae0410a069519da8a40309d9
Latte <73871195+Calliope-K@users.noreply.github.com>
2021-01-05 20:58:49 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
-#bin!bash
+#!bin/bash
+
+#evirontment
+echo "plugin L2TP.ppp">>/etc/ppp/options 
+echo "l2tpnoipsec">>/etc/ppp/options
+
+#trigger
+chmod 700 ~/path/to/l2tp.sh
+
+#start
+sudo ~/path/to/l2tp.sh
+chmod u+x run_l2tp.command
 
 #cloning
 git clone https://github.com/Calliope-K/build-tools-gcc
