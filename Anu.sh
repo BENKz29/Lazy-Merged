8c85fb509c7ca1df21a6b25191eb700a1db22694
Latte <73871195+Calliope-K@users.noreply.github.com>
2021-01-09 19:35:12 +0700

    Anu

--- Anu.sh
+++ Anu.sh
@@ -3,19 +3,22 @@
 echo " "
 echo "Merged Platform Sourced Kernel"
 
-#Connect_to_account
+echo"anu..... Cloning source"
+git clone https://github.com/Calliope-K/Tea-WIP -b WIP-4.14
+
+echo"Connect_to_account"
 git config --global user.name "Calliope-K"
 git config --global user.email "firsname76@gmail.com"
 
-#anu..... Added remoting sc and tarik sist
+echo"anu..... Added remoting sc and tarik sist"
 git remote add aurora https://source.codeaurora.org/quic/la/kernel/msm-4.14
 git fetch aurora LA.UM.8.2.1.r1-06200-sdm660.0
 
-#anu.....
+echo"anu..... Starting"
 git merge --no-commit FETCH_HEAD
 git commit -m Anu-Merged-Kramel --signoff
 
-#Push
+echo"push"
 git push https://$GITHUB_TOKEN@github.com/Calliope-K/Tea-WIP.git
 
 echo " "
