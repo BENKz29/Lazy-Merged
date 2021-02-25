ea0c46f26e86b6576d165523c843f434533973ac
ビクセル <vcyzscape@gmail.com>
2021-02-25 10:48:28 +0700

    Update Anu.sh

--- Anu.sh
+++ Anu.sh
@@ -6,13 +6,13 @@ echo "Merged Platform Sourced Kernel"
 echo " "
 echo "anu..... Cloning source"
 echo " "
-git clone https://github.com/Calliope-K/Tea-WIP -b WIP-4.14
+git clone https://github.com/vcyzteen/Tea-WIP -b WIP-4.14
 cd Tea-WIP
 
 echo " "
 echo "Connect_to_account"
 echo " "
-git config --global user.name "Calliope-K"
+git config --global user.name "vcyzteen"
 git config --global user.email "firsname76@gmail.com"
 
 echo " "
@@ -29,7 +29,7 @@ git commit -m Anu-Merged-Kramel --signoff
 
 echo "push"
 echo " "
-git push https://$GITHUB_TOKEN@github.com/Calliope-K/Tea-WIP.git
+git push https://$GITHUB_TOKEN@github.com/vcyzteen/Tea-WIP.git
 
 echo " "
 echo "Finished"
